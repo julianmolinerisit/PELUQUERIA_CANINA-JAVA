75a8d387a78485cd1b983be593b754dcb1532c45
julianmolinerisit <julianmolinerisit@gmail.com>
2024-04-21 20:27:44 -0300

    Estructura interfaz de usuario.

---
+++ src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.peluqueriacanina;
+
+
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
